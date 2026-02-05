2e12bf729be2ca9ec52eaeb915dd7d6e2a428a12
Evgeny Pihnastyi <evgeny.pihnastyi@gmail.com>
2026-02-06 01:20:55 +0300

    WR05 The initial project  reviesed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
-
+const App = () => {
 return (
-    <div className="App">
-      Simple HTML!!
-    </div>
+    <div className="App"> <span></span>
+      Simple HTML!ddd
+    </div>  
   );
 }
 
