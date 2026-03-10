863edbfdbe4a4974d1d5e8cb4e11754fd633ab3e
Evgeny Pihnastyi <evgeny.pihnastyi@gmail.com>
2026-03-10 20:46:07 +0300

    VL-10

--- src/App.js
+++ src/App.js
@@ -1,68 +1,15 @@
-import './App.css';
+import React from "react";
+import "./App.css" ;
 
+import Footer from "./Footer";
 
-const App = () => {
+ const App = () =>{
+
   return (
     <div>
-      <Header />
-      <Technologies />
+     <Footer />
     </div>
   )
 }
 
-const Technologies = () => {
-  return (
-  <div>
-    <ul>
-      <li>css</li>
-
-      <li>js</li>
-      <li>react</li>
-    </ul>
-  </div>
-  )
-}
-
-const Header = () => {
-  return (<div>
-    <a href='#s'> Home</a>
-    <a href='#s'> News Feed</a>
-    <a href='#s'>Messeges</a>
-  </div>);
-}
-
-
-const App1 = () => {
-  return (<div style={{
-    display: 'grid',
-    gridTemplateColumns: 'repeat(4, 80px)',
-    gridTemplateRows: 'repeat(4, 80px)',
-    gap: '35px',
-    margin: '50px'
-  }}>
-    <span style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'red', fontSize: '48px', fontWeight: 'bold', width: '80px', height: '80px' }}>1</span>
-    <span style={{ display: 'flex', justifyContent: 'right', alignItems: 'center', backgroundColor: 'yellow', fontSize: '24px', fontWeight: 'bold', width: '80px', height: '80px' }}>2</span>
-    <span style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'green', fontSize: '24px', fontWeight: 'bold', width: '80px', height: '80px' }}>3</span>
-    <span style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'yellow', fontSize: '24px', fontWeight: 'bold', width: '80px', height: '80px' }}>4</span>
-
-    <span style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'blue', fontSize: '24px', fontWeight: 'bold', width: '80px', height: '80px' }}>5</span>
-        <span style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'blue', fontSize: '24px', fontWeight: 'bold', width: '80px', height: '80px' }}>5</span>
-    <span style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'yellow', fontSize: '24px', fontWeight: 'bold', width: '80px', height: '80px' }}>6</span>
-    <span style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'yellow', fontSize: '24px', fontWeight: 'bold', width: '80px', height: '80px' }}>7</span>
-    <span style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'yellow', fontSize: '24px', fontWeight: 'bold', width: '80px', height: '80px' }}>8</span>
-
-    <span style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'yellow', fontSize: '24px', fontWeight: 'bold', width: '80px', height: '80px' }}>9</span>
-    <span style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'yellow', fontSize: '24px', fontWeight: 'bold', width: '80px', height: '80px' }}>10</span>
-    <span style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'yellow', fontSize: '24px', fontWeight: 'bold', width: '80px', height: '80px' }}>11</span>
-    <span style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'yellow', fontSize: '24px', fontWeight: 'bold', width: '80px', height: '80px' }}>12</span>
-
-    <span style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'yellow', fontSize: '24px', fontWeight: 'bold', width: '80px', height: '80px' }}>13</span>
-    <span style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'yellow', fontSize: '24px', fontWeight: 'bold', width: '80px', height: '80px' }}>14</span>
-    <span style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'yellow', fontSize: '24px', fontWeight: 'bold', width: '80px', height: '80px' }}>15</span>
-    <span style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'transparent', fontSize: '24px', fontWeight: 'bold', width: '80px', height: '80px' }}></span>
-  </div>
-
-  )
-}
-
-export default App
+export default App;
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,11 @@
+import React from "react";
+
 const Header = () => {
   return (<div>
-    <a href='#s'> Home</a>
-    <a href='#s'> News Feed</a>
-    <a href='#s'>Messeges</a>
-  </div>);
+      <a href="#s">Home</a>s
+      <a href="#s">News Feed</a>
+      <a href="#s">Messages</a>
+    </div>);
 }
+
+export default Header;
