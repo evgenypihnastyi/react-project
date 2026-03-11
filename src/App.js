e7ecd8369c161f8487ce13746ed175202dc0780b
Evgeny Pihnastyi <evgeny.pihnastyi@gmail.com>
2026-03-11 21:39:18 +0300

    VL-10. Уроки React JS (default опасность плагин auto import)

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React from "react";
 import "./App.css" ;
 
 import Footer from "./Footer";
+import Header from "./Header";
+import Technologies from "./Technologies";
+
+
 
  const App = () =>{
 
   return (
     <div>
+      <Header />
+      <Technologies/>
      <Footer />
     </div>
   )
 }
+
 
 export default App;
