705b1e60d745974d9b8a6424997036f183c19756
Evgeny Pihnastyi <evgeny.pihnastyi@gmail.com>
2026-04-26 21:22:14 +0300

    v-19 route, browser-router, маршрутизация

--- src/App.js
+++ src/App.js
@@ -4,17 +4,23 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 const App = (props) => {
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <Navbar />
-      {/* <Profile /> */}
-      <div class="app-wrapper-content">
-        <Dialogs />
+    <React.StrictMode>
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <Navbar />
+        <div className="app-wrapper-content">
+          <Routes>
+            <Route path= "/dialogs" component={Dialogs} />
+            <Route path= "/profile" component={Profile} />
+          </Routes>
+        </div>
       </div>
-    </div>
-    );
+    </BrowserRouter>
+  </React.StrictMode>);
 }
 
 export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,13 @@
 import React from "react";
 import s from "./Navbar.module.css";
-console.log(s);
-// let s = {
-//  "nav": "Navbar_nav__-y6P-",
-//  "item": "Navbar_item__am93N",
-//  "active" : "Baksndakdn active"
-// }
-
-let c1 = "item";
-let c2 = "active";
-// "item active"
-let classes = c1 + " " + c2;
-let classesNew = `${s.item} ${c2}`;
 
 const Navbar = () => {
         return       <nav className={s. nav}>
         <div className={s.item}>
-          <a>Profile</a>
+          <a href="/profile">Profile</a>
         </div>
       <div className={ `${s.item} ${s.active} `}>
-          <a>Messages</a>
+          <a href ="/dialogs">Messages</a>
         </div>
          <div className={s.item}>
           <a>News</a>
