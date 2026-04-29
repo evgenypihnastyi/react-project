7fc81570661bd1e2e2795d7fbd272ad98fcd9755
Evgeny Pihnastyi <evgeny.pihnastyi@gmail.com>
2026-04-29 23:21:35 +0300

    Task-019 Initial commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import classes from './Header.module.css';
+
+const Header = () => {
+    return <header className={classes.header}>                
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxMOQxU2IdDkXh_4DnrcAsfpR7UCrIozgMkg&s' />
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -0,0 +1,22 @@
+import classes from './MyPosts.module.css';
+import Post from './Post/Post';
+
+const MyPosts = () => {
+  return (
+    
+    <div >
+      My posts
+      <div>
+        <textarea></textarea>
+        <button>Add post</button>
+        <button>Remove</button>
+      </div>
+      <div className={classes.posts}>
+        <Post message='Hi, how are you?'/>
+        <Post message="It's my first post"/>        
+      </div>
+    </div>
+  )
+}
+
+export default MyPosts;
