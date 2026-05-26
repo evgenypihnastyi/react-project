a252b9116c0df5caa1a1f471e36682dc0e61bc50
Evgeny Pihnastyi <evgeny.pihnastyi@gmail.com>
2026-05-26 21:34:06 +0300

    Task-024 Added the local level of business logic

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,6 +2,13 @@ import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = (props) => {
+
+      let postData = [
+        {id: 1, messeage: "Hi, how are yuo?", likesCount: 12},
+        {id: 2, messeage: "It\'s my first post", likesCount: 11},
+
+    ]
+
   return (
     <div className={s.postsBlock}>
      <h3> My posts</h3>
@@ -12,8 +19,8 @@ const MyPosts = (props) => {
         <button>Add post</button>
       </div>
       <div className={s.posts}>
-        <Post message="Hi, how are you?" likesCount="0" />
-        <Post message="It's my first post" likesCount="23" />
+        <Post message={postData[0].message} likesCount={postData [0].likesCount}/>
+        <Post message={postData[1].message} likesCount={postData [1].likesCount}/>
       </div>
     </div>
   )
