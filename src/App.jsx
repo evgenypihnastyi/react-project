ec0c7595fe7165a31b68b8ef1d03075889bb5b8d
Evgeny Pihnastyi <evgeny.pihnastyi@gmail.com>
2026-06-23 20:33:01 +0300

    Task-031 onClick, ref, VirtualDOM

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const App = (props) => {
       <Navbar />
       <div className="app-wrapper-content">
         <Routes>
-          <Route path="/dialogs" Component={  () => <Dialogs  state={props.state.DialogsPage} /> } />                  
+          <Route path="/dialogs" Component={  () => <Dialogs  state={props.state.DialogsPage} /> } />      
           <Route path="/profile" Component={ () => <Profile   state={props.state.ProfilePage} /> } />
         </Routes>
       </div>
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,5 +1,6 @@
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
+import React from 'react';
 
 const MyPosts = (props) => {
   let post = props.post
@@ -7,14 +8,21 @@ const MyPosts = (props) => {
   let posteElement =
     post.map(p => <Post message={p.message} likesCount={p.likesCount} />)
 
+    let newPostElement = React.createRef();
+
+    let addPost = () => {
+      let text = newPostElement.current.value;
+      alert(text);
+    }
+
   return (
     <div className={s.postsBlock}>
       <h3> My posts</h3>
       <div>
-        <textarea></textarea>
+        <textarea ref={newPostElement}></textarea>
       </div>
       <div>
-        <button>Add post</button>
+        <button onClick={ addPost }>Add post</button>
       </div>
       <div className={s.posts}>
         {posteElement}
